992ecbff24a8f57da79056faf8521bc68169d36e
Jeff Putney <jeffrey.putney@gmail.com>
2013-07-20 15:21:37 -0500

    Added sample tests etc.

---
+++ src/main/java/net/certifi/audittablegen/TriggerConfiguration.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.certifi.audittablegen;
+
+/**
+ *
+ * @author jputney
+ */
+interface TriggerConfiguration {
+    public boolean skipTable(String tableName);
+}
